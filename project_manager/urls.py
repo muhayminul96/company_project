f9beccc0d8eab907029c68c0eb8e1e804e5908b8
unknown <muhayminul96@gmail.com>
2024-06-28 23:44:11 +0600

    api doc added

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -1,11 +1,27 @@
-from django.urls import path, include
+from django.urls import path, re_path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from .views import (UserViewSet, get_user_details, update_user, delete_user, register_user, LoginView, ProjectViewSet,
                     TaskViewSet, task_in_project, comments_in_task, comment_detail)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
@@ -15,6 +31,9 @@ router.register(r'tasks', TaskViewSet, basename='tasks')
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('users/register/', register_user, name='user-register'),
     path('users/login/', LoginView.as_view(), name='user-login'),
     path('users/<int:id>/', get_user_details, name='user-details'),
